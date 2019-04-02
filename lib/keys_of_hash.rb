e812d4e878f1465dc615221b19965d33b545bcaf
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-04-02 22:52:48 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,12 @@
 class Hash
   def keys_of(*arguments)
-    array = []
-    if Hash.keys == arguments
-      array << Hash.values.join
+    keys = []
+    arguments.each do |arg|
+      Hash.each do |key, values|
+        if arg == values
+          keys << key
+        end
+      end
     end
   end
 end
